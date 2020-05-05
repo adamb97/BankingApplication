1502d1f7ed1e210aee7684ea71ed43b8c239e130
Adam B <47373357+adamb97@users.noreply.github.com>
2020-05-05 17:32:47 -0400

    Initial commit
    Not yet finished

---
+++ Branch.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Branch {
+        private String name;
+        private ArrayList<Customer> customers;
+
+    public Branch(String name) {
+        this.name = name;
+        this.customers = new ArrayList<Customer>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean newCustomer(String customerName, double initialAmount){
+        if(findCustomer(customerName) == null){
+            this.customers.add(new Customer(customerName, initialAmount));
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addTransaction(String customerName, double amt){
+        Customer existingCust = findCustomer(customerName);
+
+        if(existingCust != null){
+            existingCust.addTransaction(amt);
+            return true;
+        }
+        return false;
+    }
+
+    private Customer findCustomer(String customerName){
+        for(int i =0; i<customers.size();i++){
+            Customer checkedCustomer = this.customers.get(i);
+            if(checkedCustomer.getName().equals(customerName)){
+                return checkedCustomer;
+            }
+        }
+
+        return null;
+    }
+
+
+}
